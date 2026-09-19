3f11e513e1cd26db8c22e2b884f2975faaec3fd3
lasersushi <lwalker30@priorypanther.com>
2026-09-19 16:57:25 -0700

    feat: Creating parts 1a and 1b

---
+++ src/Homework5.java
@@ -0,0 +1,44 @@
+/* 
+Lucas Walker
+Mr. Menezes
+AT CS - C
+9/19/26
+*/
+
+public class Homework5 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void drawLine(int x1, int y1, int x2, int y2) {
+        if (x1 < 0 || x1 > 10 || y1 < 0 || y1 > 10 || x2 < 0 || x2 > 10 || y2 < 0 || y2 > 10) {
+            // Invalid coordinate provided, print an error message to the user
+            System.out.println("One of the input coordinates is not in the 10x10 grid");
+        } else {
+            // Draws line in hidden grid, print confirmation message to the user
+            System.out.println("Drew a line from (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ")");
+        }
+    }
+
+    public static void respondToRSVP(boolean rsvp, int selection, String option1, String option2) {
+        String choice = null;
+        if (selection == 1) {
+            choice = "beef";
+        }
+        else if (selection == 2) {
+            choice = "chicken";
+        }
+        else if (selection == 3) {
+            choice = "pasta";
+        }
+        else {
+            choice = "fish";
+        }
+        if (rsvp) {
+            System.out.println("attending");
+        }
+        else {
+            System.out.println("not attending");
+        }
+    }
+}
